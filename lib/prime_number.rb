2f2160d702f8232da4f6a753cd9a298bcc56f595
Stuart Bates <stuart@builtbyclick.com>
2015-09-29 14:52:43 +0100

    Implementation of Eratosthenes Sieve

--- lib/prime_number.rb
+++ lib/prime_number.rb
@@ -18,7 +18,40 @@ class PrimeNumber
   end
 
   def [](index)
+    generate_primes while @primes.size <= index
     @primes[index]
+  end
+
+  # Based on: https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+  def generate_primes
+    batch = generate_batch(@primes, 1e6)
+    break_point = calculate_break_point(batch.last)
+    batch_min = batch.first
+
+    @primes.each do |prime|
+      break if prime > break_point
+      index = calculate_index_of_composite(prime, batch_min)
+      while index < batch.size do
+        batch[index] = nil
+        index += prime
+      end
+    end
+
+    @primes.concat(batch.compact!)
+  end
+
+  def generate_batch(primes, size)
+    start_of_batch = primes.last + 2
+    end_of_batch = start_of_batch + size
+    (start_of_batch..end_of_batch).to_a
+  end
+
+  def calculate_break_point(maximum)
+    Math.sqrt(maximum).floor.to_i
+  end
+
+  def calculate_index_of_composite(prime, batch_min)
+    prime - (batch_min % prime)
   end
 
 end
--- spec/prime_number_spec.rb
+++ spec/prime_number_spec.rb
@@ -14,10 +14,45 @@ describe 'PrimeNumber' do
 
   describe '#[]' do
 
-    it 'should return the nth prime number' do
-      expect(prime[9]).to eq(29)
+    context 'asking for 10th prime number' do
+      it 'should return 29' do
+        expect(prime[9]).to eq(29)
+      end
     end
 
+    context 'asking for 500th prime number' do
+      it 'should return 3571' do
+        expect(prime[499]).to eq(3571)
+      end
+    end
+
+  end
+
+  describe '#generate_batch' do
+
+    it 'generates an array of sequential integers to sieve through' do
+      batch = prime.send(:generate_batch, [2,3,5], 100)
+      expect(batch.first).to eq(7)
+      expect(batch.last).to eq(107)
+    end
+
+  end
+
+  describe '#calculate_break_point' do
+
+    it 'returns the square root of the batch maximum' do
+      expect(prime.calculate_break_point(120)).to eq(10)
+      expect(prime.calculate_break_point(122)).to eq(11)
+    end
+
+  end
+
+  describe '#calculate_index_of_composite' do
+    context 'for batch starting at 103' do
+      it 'returns the index of the next multiple of 2' do
+        expect(prime.send(:calculate_index_of_composite, 2, 103)).to eq(1)
+      end
+    end
   end
 
 end
